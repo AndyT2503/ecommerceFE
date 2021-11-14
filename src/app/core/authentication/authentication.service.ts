082ad1cdb458ea19fc7850fc3d50fffa601ef310
tuhoang <tu.hoang@gosmartlog.com>
2021-11-14 21:13:59 +0700

    bugfix/refresh-token

--- src/app/core/authentication/authentication.service.ts
+++ src/app/core/authentication/authentication.service.ts
@@ -67,14 +67,14 @@ export class AuthenticationService {
           headers: header
         });
       }),
+      tap(res => {
+        this.connectSocket();
+        this.authenticationStore.update({userProfile: {...res, isAuthenticate: true}});
+      }),
       catchError(() => {
         this.disconnectSocket();
         this.authenticationStore.reset();
         return EMPTY;
-      }),
-      tap(res => {
-        this.connectSocket();
-        this.authenticationStore.update({userProfile: {...res, isAuthenticate: true}});
       }),
     );
   }
@@ -99,9 +99,9 @@ export class AuthenticationService {
   }
 
   private parseJwt (token: string): JwtModel {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
 
